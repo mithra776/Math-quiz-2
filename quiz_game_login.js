df4584ec3d0d0b42af76a2e6a165dc251179a5f3
mithra776 <85687745+mithra776@users.noreply.github.com>
2021-06-13 17:38:07 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,10 @@
+function addUser()
+{
+    player1 = document.getElementById("player1_name_input").value;
+    player2 = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1" , player1);
+    localStorage.setItem("player2" , player2);
+
+    window.location= "game_page.html";
+}
